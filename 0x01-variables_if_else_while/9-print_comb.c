c574843bbcbda82f7870d076f86bb0386346044f
bouilla <bouilla.abdellah@gmail.com>
2023-03-20 18:36:44 -0700

    Modified 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('0' + x);
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+
+	return (0);
+}
