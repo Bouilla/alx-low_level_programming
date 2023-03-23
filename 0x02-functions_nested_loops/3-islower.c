1058dff1b47052ef1c14dfe519f39f5557b065b7
bouilla <bouilla.abdellah@gmail.com>
2023-03-22 17:17:43 -0700

    message

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Check Main
+ * @c: An input character
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122 )
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
+	_putchar('\n'); 
+}
