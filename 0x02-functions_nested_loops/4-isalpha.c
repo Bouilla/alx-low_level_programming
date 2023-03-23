b734687e2f90fc47ce6e3003bd9f6b0d81354309
bouilla <bouilla.abdellah@gmail.com>
2023-03-22 17:54:10 -0700

    message

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <ctype.h>
-
 /**
  * _isalpha - Check Main
  * @c: An input character
@@ -10,7 +8,7 @@
  */
 int _isalpha(int c)
 {
-	if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
