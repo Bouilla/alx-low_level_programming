bdebc53579cae0206a099dd98551823e77468333
bouilla <bouilla.abdellah@gmail.com>
2023-03-27 17:22:23 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+void puts2(char *str)
+{
+	int len = 0;
+	int a = 0;
+	char *s = str;
+	int st;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	a = len - 1;
+	for (st = 0 ; st <= a ; st++)
+	{
+		if (st % 2 == 0)
+		{
+			_putchar(str[st]);
+		}
+	}
+	_putchar('\n');
+}
