80f4099316cf233c942724bc77c72a600b558e47
bouilla <bouilla.abdellah@gmail.com>
2023-04-09 01:38:43 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
   *main - Entery point
   *
   *_puts_recursion a function that prints a string in reverse.
-  *@sthe string to be printed
+  *@s the string to be printed
  */
 void _puts_recursion(char *s)
 {
