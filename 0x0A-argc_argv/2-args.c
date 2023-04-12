ab2aa0d717eb32969000737377cca37851e828ff
bouilla <bouilla.abdellah@gmail.com>
2023-04-12 07:22:40 -0700

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,8 +9,9 @@
 */
 int main(int argc, char *argv[])
 {
-	int j;
-	for (j = 0; j < argc; j++)
-		printf("%s\n", argv[j]);
-	return (0);
+
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
 }
