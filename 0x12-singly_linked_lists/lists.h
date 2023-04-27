073421536f88ad0ec488da2e5046650be2295656
Bouilla <Bouilla.abdellah@gmail.com>
2023-04-27 13:36:37 -0700

    print-list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,35 +1,28 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_
+#define LISTS_H_
 
-/**
- *  * File: lists.h
- *   * Auth: Florence Hanson
- *     * Desc: Header file containing prototypes and definition
- */
-
+#include <stddef.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
+ * struct list_s - Singly linked lists
+ * @str: string
+ * @len: cp
+ * @next: points to node
+ *
+ * Description: Singly linked lists
  */
-
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-}
-list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
+void free_list(list_t *);
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
-#endif /* LISTS_H */
+#endif
